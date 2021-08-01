89da5da9768d93b91d65358cc40e12fec0f88f48
RavenBlack <ravenblack@gmail.com>
2021-08-01 19:06:27 -0400

    Initial version. Supports the field types I'm using right now.

---
+++ README.md
@@ -0,0 +1,134 @@
+# kserialc
+
+This is a lightweight script to generate serializable classes from a schema.
+
+`kserial` is the companion project that allows the generated classes to
+serialize and deserialize.
+
+## Installation
+
+```
+npm install -g kserialc
+```
+
+## Usage
+
+```
+kserialc < my_message_types.k > my_message_types.ts
+```
+
+## k schema format
+
+The schema is similar to the flatbuffers schema.
+
+### enum
+
+Generates an enum type that can be used in the serializable messages.
+
+The `<byte|uint16|uint32>` value determines the representation of the 
+enum in the serialized data.
+
+```
+enum <name> : <byte|uint16|uint32> {
+  VALUE1,
+  VALUE2,
+  [...]
+}
+```
+
+### table
+
+Generates a serializable class named `<name>`. The four character ID must
+be unique across all messages within the same project - it is used to
+parse messages received in an `any` type.
+
+```
+table <name> : <four character id> {
+  <field1name>: <field1type>;
+  <field2name>: <field2type>;
+  [...]
+}
+```
+
+#### Possible field types
+
+`uint16`, `uint32`, `int16`, `int32`, `byte`
+
+The basic types, each taking up the obvious fixed number of bytes. They all map to `number` in typescript.
+
+`string`
+
+The other basic type. Takes up 4 bytes in the base table even if the string
+is empty, plus 4 bytes in the dynamic area for the length if nonzero,
+plus however many bytes it takes to represent the string in utf8.
+
+`your_enum_type`
+
+Takes up the number of bytes in the base table as selected for the enum type.
+
+`your_table_type`
+
+A single submessage.
+
+Takes up 4 bytes in the base table even if null, plus 4 bytes in the dynamic
+area for the length if nonzero, plus however many bytes the submessage
+serializes to.
+
+`[your_table_type]`
+
+An array of the same type of submessage.
+
+Takes up 4 bytes in the base table even if empty, plus 4 bytes in the dynamic
+area for the length of the array, plus 4 bytes for each message for its
+offset, plus 4 bytes for the offset to the end of the last message, plus
+the combined size of the submessages.
+
+`any`
+
+A single submessage dynamically typed.
+
+Takes up 4 bytes in the base table even if null, plus 4 bytes in the dynamic
+area for the length if nonzero, plus 4 bytes for the id, plus however many
+bytes the submessage serializes to.
+
+
+### Example schema
+
+```
+enum TestEnum : byte { X, Y, SOMETHING, Z };
+
+table TestMsg : test {
+  name: string;
+  age: byte;
+  something: TestEnum;
+  whatever: int32;
+  herp: uint32;
+  derp: int16;
+  glurp: uint16;
+  pleaseDontRecurse: TestMsg;
+  dontRecurseHereEither: [TestMsg];
+  norInAnAny: any;
+}
+
+table TestMsgJustStr : tstr {
+  str: string;
+}
+
+table TestMsgNoFlex : tnof {
+  byte: byte;
+  byteEnum: TestEnum;
+  int32: int32;
+  uint32: uint32;
+  int16: int16;
+  uint16: uint16;
+}
+```
+
+Note, it is possible to create a recursive message as a message value
+can be populated with itself, in the simplest case. If you do this, you
+will create a stack overflow.
+
+However, it is *not* possible to send a recursive message in the form of
+the serialized binary, because each message may only refer to its own
+contents - any offset that attempts to point outside of the message will
+cause an exception to be thrown during deserialize.
---
+++ cli.js
@@ -0,0 +1,167 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+
+const src = fs.readFileSync(0, 'utf-8').replace(/\/\/[^\n]*/g, '');
+
+let pos = 0;
+const nextCmdRE = /[\s;]*([a-zA-Z0-9_]+)/;
+const enumRE = /\s*([a-zA-Z0-9_]+)\s*:\s*(byte|u?int16|u?int32)\s*\{([^\}]+)\}/;
+const tableRE = /\s*([a-zA-Z0-9_]+)\s*:\s*([a-zA-Z0-9_]{4})\s*\{([^\}]*)\}/;
+
+const enumTypes = {};
+const tableTypes = {};
+const enumValues = {};
+const commonTypeSizes = {'byte':1, 'uint16':2, 'uint32':4, 'int16': 2, 'int32': 4, 'string':4, 'any':4};
+const commonTypeMappings = {'byte':'number', 'uint16':'number', 'uint32':'number','int16':'number','int32':'number','any':'KSerializableAny|null','string':'string'};
+const commonTypeDefaults = {'byte':'0', 'uint16': '0', 'uint32': '0', 'int16': '0', 'int32': '0', 'string': "''"};
+
+function cap(s) {
+ return s.charAt(0).toUpperCase()+s.substring(1);
+}
+
+function localTypeTransform(t) {
+ if (commonTypeMappings[t]) return commonTypeMappings[t];
+ if (enumTypes[t]) return t;
+ if (t.endsWith('[]')) return t;
+ return `${t}|null`;
+}
+
+function transformDefault(t) {
+ if (commonTypeDefaults[t]) return commonTypeDefaults[t];
+ if (enumTypes[t]) return `${t}.${enumValues[t][0]}`;
+ if (t.endsWith('[]')) return '[]';
+ return 'null';
+}
+
+function typeLength(t) {
+ if (commonTypeSizes[t]) return commonTypeSizes[t];
+ if (enumTypes[t]) return commonTypeSizes[enumTypes[t]];
+ if (tableTypes[t]) return 4;
+ if (t.endsWith('[]') && tableTypes[t.substr(0, t.length-2)]) return 4;
+ throw(`unrecognized type ${t} - types must be declared before they are used`);
+}
+
+function serializeLengthFor(type,name) {
+ if (type=='string') return `stringLength(this.${name})`;
+ if (type=='any') return `anyLength(this.${name})`;
+ if (commonTypeSizes[type]) return null;
+ if (enumTypes[type]) return null;
+ if (type.endsWith('[]')) return `subArrayLength(this.${name})`;
+ return `subLength(this.${name})`;
+}
+
+function serializeFor(type,name,offset) {
+ if (enumTypes[type]) type = enumTypes[type];
+ if (type=='string') return `offset += serializeString(dest, ${offset}, offset, this.${name});`;
+ if (type=='any') return `offset += serializeAny(dest, ${offset}, offset, this.${name});`;
+ if (commonTypeSizes[type]) return `serialize${cap(type)}(dest, ${offset}, this.${name});`;
+ if (type.endsWith('[]')) return `offset += serializeSubArray(dest, ${offset}, offset, this.${name});`;
+ return `offset += serializeSub(dest, ${offset}, offset, this.${name});`;
+}
+
+function deserializeFor(type,name,offset) {
+ if (enumTypes[type]) return `${name}: deserialize${cap(enumTypes[type])}(src, ${offset}) as ${type}`;
+ if (commonTypeSizes[type]) return `${name}: deserialize${cap(type)}(src, ${offset})`;
+ if (type.endsWith('[]')) return `${name}: deserializeSubArray(${type.substring(0, type.length - 2)}.deserialize, src, ${offset}) as ${type}`;
+ return `${name}: deserializeSub(${type}.deserialize, src, ${offset}) as ${type}`;
+}
+
+console.log(`import {KSerializableAny,KSerializable,register,
+subLength,anyLength,subArrayLength,stringLength,
+serializeSub,serializeSubArray,serializeInt32,serializeInt16,serializeUint32,serializeUint16,serializeByte,serializeString,serializeAny,
+deserializeSub,deserializeSubArray,deserializeInt32,deserializeInt16,deserializeUint32,deserializeUint16,deserializeByte,deserializeString,deserializeAny
+} from 'kserial';\n`);
+
+for (let match; pos != -1 && (match = src.substr(pos).match(nextCmdRE)) !== null;) {
+ pos += match[0].length;
+ switch (match[1]) {
+  case 'namespace': pos = src.indexOf(';', pos); break;
+  case 'enum': {
+   const enumMatch = src.substr(pos).match(enumRE);
+   if (enumMatch == null) throw(`failed to match enum <name> : <byte|uint16|uint32|int16|int32> {values} at ${src.substr(pos, 80)}`);
+   const name = enumMatch[1];
+   const type = enumMatch[2];
+   const values = enumMatch[3].split(',').map((v) => v.trim()).filter((v) => v.length > 0);
+   enumTypes[name]=type;
+   enumValues[name]=values;
+   console.log(`export enum ${name} {`);
+   for (const value of values) {
+    if (!value.match(/^[A-Za-z0-9_]+$/)) {
+     throw(`invalid enum name: $value`);
+    }
+    console.log(`  ${value},`);
+   }
+   console.log('}\n');
+   pos += enumMatch[0].length;
+   break;
+  }
+  case 'table': {
+   const tableMatch = src.substr(pos).match(tableRE);
+   if (tableMatch === null) throw(`failed to match table <name>:<id> {<values>} at ${src.substr(pos, 80)}`);
+   const name = tableMatch[1];
+   const id = tableMatch[2];
+   const values = tableMatch[3].split(';');
+   tableTypes[name]=true;
+   pos += tableMatch[0].length;
+   console.log(`export class ${name} {`);
+   console.log(`  static readonly id:string = '${id}';`);
+   const names=[];
+   const namesWithDefaults=[];
+   const types=[];
+   const localTypes=[];
+   const namesWithTypes=[];
+   for (const value of values) {
+    if (value.trim().length === 0) continue;
+    const nameTypeMatch = value.match(/^\s*([a-zA-Z0-9_]+)\s*:\s*\[?([a-zA-Z0-9_]+)(\]?)$/);
+    if (nameTypeMatch === null) throw(`failed to match '<name> : <type>' in values of table ${name}: '${value}'`);
+    names.push(nameTypeMatch[1]);
+    types.push(nameTypeMatch[2]+(nameTypeMatch[3]?'[]':''));
+    localTypes.push(localTypeTransform(types[types.length-1]));
+    namesWithDefaults.push(`${nameTypeMatch[1]}=${transformDefault(types[types.length-1])}`);
+    namesWithTypes.push(`${nameTypeMatch[1]}?:${localTypes[localTypes.length-1]}`);
+    console.log(`  ${nameTypeMatch[1]}: ${localTypes[localTypes.length-1]};`);
+   }
+   console.log(`  constructor({${namesWithDefaults.join(',')}}:{${namesWithTypes.join(',')}}={}) {${names.map((n) => `this.${n}=${n};`).join(' ')}}`);
+   const typeLengths = types.map(typeLength);
+   const baseLength = typeLengths.reduce((a, b) => a+b, 0);
+   const serializeExtraLengths = [baseLength];
+   const serializes = [];
+   const deserializes = [];
+   let o = 0;
+   for (let i = 0; i < types.length; i++) {
+    serializes.push(serializeFor(types[i], names[i], o));
+    deserializes.push(deserializeFor(types[i], names[i], o));
+    o += typeLengths[i];
+    const fn = serializeLengthFor(types[i], names[i]);
+    if (fn === null) continue;
+    serializeExtraLengths.push(fn);
+   }
+   console.log(`  get serializeLength(): number { return ${serializeExtraLengths.join('+')}; }`);
+   console.log('  serialize(dest: Uint8Array): number {');
+   if (serializes.some((s) => s.includes('offset +='))) {
+    console.log(`    let offset = ${baseLength};`);
+   }
+   for (const serialize of serializes) {
+    console.log(`    ${serialize}`);
+   }
+   if (serializes.some((s) => s.includes('offset +='))) {
+    console.log(`    return offset;`);
+   } else {
+    console.log(`    return ${baseLength};`);
+   }
+   console.log('  }');
+   
+   console.log(`  static deserialize(src: Uint8Array): ${name} {`);
+   console.log(`    return new ${name}({`);
+   for (const deserialize of deserializes) {
+     console.log(`      ${deserialize},`);
+   }
+   console.log('    });');
+   console.log('  }');
+   console.log('}');
+   console.log(`register('${id}', ${name}.deserialize);\n`);
+   break;
+  }
+ }
+}
